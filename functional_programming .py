bc3ae587268dd0228e52430e49969c8d929b1750
Daniel Briquez <102300060+Briqz23@users.noreply.github.com>
2022-09-24 16:47:01 -0300

    01

---
+++ functional_programming .py
@@ -0,0 +1,10 @@
+i =  map(lambda x:x**3, range(5))
+print(list(i))
+
+j = filter(lambda x:x>3, range(10))
+print(list(j))
+
+ingredientes1 ,estado = ['tomate', 'ovo', 'carne'], ['fresco','cozido', 'mal-passada']
+
+k = map(lambda x,y, z: x+y+z, ingredientes1, [' ']*len(estado), estado)
+print(list(k))
